60a9dc916bbbbd0ffa492a3424f54b485dba1b3b
LUSA <107564399+LooosaZ@users.noreply.github.com>
2024-05-30 12:51:37 +0100

    added cors to start on frontend

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const http = require('http');
 const config = require("./config");
 require('dotenv').config();
+const cors = require("cors");
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 let router = require('./router');
 const { default: mongoose } = require('mongoose');
 var app = express();
+app.use(cors());
 app.use(router.initialize());
   
 const server = http.Server(app);
